96ad2878d6d86bd8a31040d96078a93d36c9a248
Jonathan <z5310747@ad.unsw.edu.au>
2021-04-05 15:03:09 +1000

    Finished dm_messages tests for http

--- http_tests/dm_http_test.py
+++ http_tests/dm_http_test.py
@@ -488,3 +488,268 @@ def test_http_dm_message_working():
     assert dmMessagesR == {'messages': [], 'start': 0, 'end': -1}
     # ----------------------------
 
+def test_http_dm_message_invalid_user():
+    requests.delete(config.url + "clear/v1")
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user = requests.post(config.url + funcURL, json=inputData)
+    userR = json.loads(user.text)
+    # ----------------------------
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test2@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user2 = requests.post(config.url + funcURL, json=inputData)
+    user2R = json.loads(user2.text)
+    # ----------------------------
+    # Creating DM-----------------
+    funcURL = "dm/create/v1"
+    userList = []
+    userList.append(userR["auth_user_id"])
+    userList.append(user2R["auth_user_id"])
+    inputData = {
+        "token": userR["token"],
+        "u_ids": userList,
+    }
+    dm = requests.post(config.url + funcURL, json=inputData)
+    dmR = json.loads(dm.text)
+    # ----------------------------
+    temp = jwt.encode({"sessionId": 2}, secretSauce, algorithm = "HS256")
+    # Checking Messages-----------------
+    funcURL = "dm/messages/v1"
+    inputData = {
+        "token": temp,
+        "dm_id": dmR["dm_id"],
+        "start": 0,
+    }
+    qData = urllib.parse.urlencode(inputData)
+    dmMessages = requests.get(config.url + funcURL + "?" + qData)
+    dmMessagesR = json.loads(dmMessages.text)
+    assert dmMessagesR["code"] == 403
+    # ----------------------------
+def test_http_dm_message_invalid_dm():
+    requests.delete(config.url + "clear/v1")
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user = requests.post(config.url + funcURL, json=inputData)
+    userR = json.loads(user.text)
+    # ----------------------------
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test2@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user2 = requests.post(config.url + funcURL, json=inputData)
+    user2R = json.loads(user2.text)
+    # ----------------------------
+    # Creating DM-----------------
+    funcURL = "dm/create/v1"
+    userList = []
+    userList.append(userR["auth_user_id"])
+    userList.append(user2R["auth_user_id"])
+    inputData = {
+        "token": userR["token"],
+        "u_ids": userList,
+    }
+    dm = requests.post(config.url + funcURL, json=inputData)
+    dmR = json.loads(dm.text)
+    # ----------------------------
+    # Checking Messages-----------------
+    funcURL = "dm/messages/v1"
+    inputData = {
+        "token": userR["token"],
+        "dm_id": 999,
+        "start": 0,
+    }
+    qData = urllib.parse.urlencode(inputData)
+    dmMessages = requests.get(config.url + funcURL + "?" + qData)
+    dmMessagesR = json.loads(dmMessages.text)
+    assert dmMessagesR["code"] == 400
+    # ----------------------------
+
+def test_http_dm_message_invalid_dm():
+    requests.delete(config.url + "clear/v1")
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user = requests.post(config.url + funcURL, json=inputData)
+    userR = json.loads(user.text)
+    # ----------------------------
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test2@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user2 = requests.post(config.url + funcURL, json=inputData)
+    user2R = json.loads(user2.text)
+    # ----------------------------
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test3@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user3 = requests.post(config.url + funcURL, json=inputData)
+    user3R = json.loads(user3.text)
+    # ----------------------------
+    # Creating DM-----------------
+    funcURL = "dm/create/v1"
+    userList = []
+    userList.append(userR["auth_user_id"])
+    userList.append(user2R["auth_user_id"])
+    inputData = {
+        "token": userR["token"],
+        "u_ids": userList,
+    }
+    dm = requests.post(config.url + funcURL, json=inputData)
+    dmR = json.loads(dm.text)
+    # ----------------------------
+    # Checking Messages-----------------
+    funcURL = "dm/messages/v1"
+    inputData = {
+        "token": user3R["token"],
+        "dm_id": dmR["dm_id"],
+        "start": 0,
+    }
+    qData = urllib.parse.urlencode(inputData)
+    dmMessages = requests.get(config.url + funcURL + "?" + qData)
+    dmMessagesR = json.loads(dmMessages.text)
+    assert dmMessagesR["code"] == 403
+    # ----------------------------
+
+def test_http_dm_message_startgreater():
+    requests.delete(config.url + "clear/v1")
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user = requests.post(config.url + funcURL, json=inputData)
+    userR = json.loads(user.text)
+    # ----------------------------
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test2@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user2 = requests.post(config.url + funcURL, json=inputData)
+    user2R = json.loads(user2.text)
+    # ----------------------------
+    # Creating DM-----------------
+    funcURL = "dm/create/v1"
+    userList = []
+    userList.append(userR["auth_user_id"])
+    userList.append(user2R["auth_user_id"])
+    inputData = {
+        "token": userR["token"],
+        "u_ids": userList,
+    }
+    dm = requests.post(config.url + funcURL, json=inputData)
+    dmR = json.loads(dm.text)
+    # ----------------------------
+    # Checking Messages-----------------
+    funcURL = "dm/messages/v1"
+    inputData = {
+        "token": userR["token"],
+        "dm_id": dmR["dm_id"],
+        "start": 1,
+    }
+    qData = urllib.parse.urlencode(inputData)
+    dmMessages = requests.get(config.url + funcURL + "?" + qData)
+    dmMessagesR = json.loads(dmMessages.text)
+    assert dmMessagesR["code"] == 400
+    # ----------------------------
+
+def test_http_dm_message_endnegativeone():
+    requests.delete(config.url + "clear/v1")
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user = requests.post(config.url + funcURL, json=inputData)
+    userR = json.loads(user.text)
+    # ----------------------------
+
+    # Register--------------------
+    funcURL = "auth/register/v2"
+    inputData = {
+        "email": "test2@hotmail.com",
+        "password": "password1",
+        "name_first": "nameFirst",
+        "name_last": "nameLast",
+    }
+    user2 = requests.post(config.url + funcURL, json=inputData)
+    user2R = json.loads(user2.text)
+    # ----------------------------
+    # Creating DM-----------------
+    funcURL = "dm/create/v1"
+    userList = []
+    userList.append(userR["auth_user_id"])
+    userList.append(user2R["auth_user_id"])
+    inputData = {
+        "token": userR["token"],
+        "u_ids": userList,
+    }
+    dm = requests.post(config.url + funcURL, json=inputData)
+    dmR = json.loads(dm.text)
+    # ----------------------------
+    # Checking Messages-----------------
+    funcURL = "dm/messages/v1"
+    inputData = {
+        "token": userR["token"],
+        "dm_id": dmR["dm_id"],
+        "start": 0,
+    }
+    qData = urllib.parse.urlencode(inputData)
+    dmMessages = requests.get(config.url + funcURL + "?" + qData)
+    dmMessagesR = json.loads(dmMessages.text)
+    assert dmMessagesR["end"] == -1
+    # ----------------------------
